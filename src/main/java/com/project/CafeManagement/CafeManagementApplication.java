ac9ca980d4825fa52ad075323e6a643764272c34
shubham8080143 <shubhamthorat5041@gmail.com>
2025-01-11 20:05:35 +0530

    no message

--- src/main/java/com/project/CafeManagement/CafeManagementApplication.java
+++ src/main/java/com/project/CafeManagement/CafeManagementApplication.java
@@ -2,10 +2,9 @@ package com.project.CafeManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.project.CafeManagement")
+//@ComponentScan(basePackages = "com.project.CafeManagement")
 public class CafeManagementApplication {
 
 	public static void main(String[] args) {
